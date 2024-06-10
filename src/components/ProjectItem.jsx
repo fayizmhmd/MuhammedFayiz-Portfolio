054dcb6d09c10b22136930ea860e50928d587129
fayizmhmd <fayizklr192@gmail.com>
2024-06-10 11:37:34 +0530

    changes in projects

--- src/components/ProjectItem.jsx
+++ src/components/ProjectItem.jsx
@@ -14,9 +14,9 @@ const ProjectItem = ({ img, title }) => {
         <h3 className="text-2xl font-bold text-white tracking-wider text-center">
           {title}
         </h3>
-        <p className="pb-4 pt-2 text-white text-center">React Js</p>
+        {/* <p className="pb-4 pt-2 text-white text-center">React Js</p> */}
         <a href="">
-          <p className="text-center p-3 rounded-lg bg-white text-gray-400 font-bold cursor-pointer text-lg">
+          <p className="text-center p-3 rounded-lg bg-white text-gray-400 font-bold cursor-pointer text-lg mt-5">
             More Info
           </p>
         </a>
